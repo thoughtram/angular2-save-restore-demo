1cccee7d6cef230850b647377d92ddb69de0ac32
Christoph Burgdorf <christoph.burgdorf@bvsn.org>
2015-09-14 18:09:27 +0200

    feat: don't include editor in card

--- src/hero-card-component.ts
+++ src/hero-card-component.ts
@@ -1,5 +1,4 @@
 import {Component, View, bootstrap, CORE_DIRECTIVES} from 'angular2/angular2';
-import {HeroEditorComponent} from './hero-editor-component';
 import {Hero} from './hero';
 
 @Component({
@@ -11,27 +10,9 @@ import {Hero} from './hero';
     <div>
       <span>Name:</span>
       <span>{{hero.name}}</span>
-      <button [hidden]="editMode" (click)="editMode = !editMode">edit</button>
-    </div>
-    <hero-editor-component
-      (saved)="saved($event)"
-      (canceled)="canceled($event)"
-      [hidden]="!editMode"
-      [hero]="hero">
-    </hero-editor-component>
-    `,
-  directives: [CORE_DIRECTIVES, HeroEditorComponent]
+    </div>`,
+  directives: [CORE_DIRECTIVES]
 })
 export class HeroCardComponent {
   hero: Hero;
-  editMode: boolean
-
-  saved (updatedHero: Hero) {
-    this.hero = updatedHero;
-    this.editMode = false;
-  }
-
-  canceled (previousHero: Hero) {
-    this.editMode = false;
-  }
 }
--- src/heroes-list-component.ts
+++ src/heroes-list-component.ts
@@ -1,6 +1,7 @@
 import {Component, View, bootstrap, CORE_DIRECTIVES} from 'angular2/angular2';
 import {HeroesService} from './heroes-service';
 import {HeroCardComponent} from './hero-card-component';
+import {HeroEditorComponent} from './hero-editor-component';
 import {Hero} from './hero';
 
 @Component({
@@ -10,18 +11,41 @@ import {Hero} from './hero';
   template: `
     <div>
       <ul>
-        <li *ng-for="#hero of heroes">
-          <hero-card-component [hero]="hero"></hero-card-component>
+        <li *ng-for="#editItem of heroes">
+          <hero-card-component [hidden]="editItem.editing" [hero]="editItem.item"></hero-card-component>
+          <button [hidden]="editItem.editing" (click)="editItem.editing = true">edit</button>
+          <hero-editor-component
+            (saved)="onSaved(editItem, $event)"
+            (canceled)="onCanceled(editItem)"
+            [hidden]="!editItem.editing"
+            [hero]="editItem.item">
+          </hero-editor-component>
         </li>
       </ul>
     </div>`,
-  directives: [CORE_DIRECTIVES, HeroCardComponent]
+  directives: [CORE_DIRECTIVES, HeroCardComponent, HeroEditorComponent]
 })
 export class HeroesListComponent {
   heroes: Array<Hero>;
   constructor(heroesService: HeroesService) {
-    this.heroes = heroesService.getHeroes();
+    this.heroes = heroesService.getHeroes()
+                               .map(item => new EditItem(item));
   }
+
+  onSaved (editItem: EditItem<Hero>, updatedHero: Hero) {
+    editItem.item = updatedHero;
+    editItem.editing = false;
+  }
+
+  onCanceled (editItem: EditItem<Hero>) {
+    editItem.editing = false;
+  }
+}
+
+class EditItem<T> {
+  item: T;
+  editing: boolean
+  constructor (public item T) {}
 }
 
 bootstrap(HeroesListComponent, [HeroesService]);
